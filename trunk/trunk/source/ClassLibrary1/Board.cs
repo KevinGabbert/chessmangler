23dab06d9b5b314e6138750a5ead7df34ea23ab7
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-03-31 04:06:51 +0000

    fixed properties, removed de-referenced code

--- trunk/trunk/source/ClassLibrary1/Board.cs
+++ trunk/trunk/source/ClassLibrary1/Board.cs
@@ -1,88 +1,96 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-//namespace Board
-//{
-//    public class Square                   
-//    {
-//        public Square(int x, int y)
-//        {
-//            int[] position = new int[2] { x, y };
-//        }
-//        public int black
-//        {
-//            get
-//            { return black; }
-//            set
-//            { black = value; }
-//        }
-//        public int white
-//        {
-//            get
-//            { return white; }
-//            set
-//            { white = value; }
-//        }
-//        public string name
-//        {
-//            get
-//            { return name; }
-//            set
-//            { this.name = value; }
-//        }
-//        public int number
-//        {
-//            get
-//            { return this.number; }
-//            set
-//            { this.number = value; }
-//        }
-//        public int color
-//        {
-//            get
-//            { return this.color; }
-//            set
-//            { this.color = value; }
-//        }
-//        public int piece
-//        {
-//            get
-//            { return this.piece; }
-//            set
-//            { piece = value; }
-//        }        
-//    }
+namespace Board
+{
+    public class Square
+    {
+        public Square(int x, int y)
+        {
+            int[] position = new int[2] { x, y };
+        }
 
-//    public class board
-//    {
-//        board(int x, int y)
-//        {
-//            // constructor takes two ints, makes the board
-//            Square[,] board = new Square[x, y];
+        int _color;
+        int _piece;
+        int _number;
+        string _name;
 
-//            //initialize all squares created
+        public string Name
+        {
+            get
+            { 
+                return _name; 
+            }
+            set
+            { 
+                _name = value; 
+            }
+        }
+        public int Number
+        {
+            get
+            { 
+                return _number; 
+            }
+            set
+            { 
+                _number = value; 
+            }
+        }
+        public int Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+        public int Piece
+        {
+            get
+            { 
+                return _piece; 
+            }
+            set
+            { 
+                _piece = value; 
+            }
+        }
+    }
 
-//            for (int row = 0; row == x; row++)
-//            {
-//                for (int column = 0; column == y; column++)
-//                {
-//                    board[row, column] = new Square(row,column);
-//                    board[row, column].piece = 0;
-//                    board[row, column].black = 0;
-//                    board[row, column].white = 0;
-//                    board[row, column].number = (x*y)+x;
-//                    board[row, column].name = (char)(97+row)+column.ToString();
-                    
-//                    if (((board[row, column].number)%2)==0)
-//                    {board[row, column].color = 1;}
-//                    else
-//                    {board[row, column].color = 0;}
-//                }
-//            }           
+    public class board
+    {
+        board(int x, int y)
+        {
+            // constructor takes two ints, makes the board
+            Square[,] board = new Square[x, y];
 
+            //initialize all squares created
 
-//        }
-//    }     
-//}
+            for (int row = 0; row == x; row++)
+            {
+                for (int column = 0; column == y; column++)
+                {
+                    board[row, column] = new Square(row, column);
+                    board[row, column].Piece = 0;
+                    board[row, column].Number = (x * y) + x;
+                    board[row, column].Name = (char)(97 + row) + column.ToString();
+
+                    if (((board[row, column].Number) % 2) == 0)
+                    {
+                        board[row, column].Color = 1;
+                    }
+                    else
+                    {
+                        board[row, column].Color = 0;
+                    }
+                }
+            }
+        }
+    }
+}
