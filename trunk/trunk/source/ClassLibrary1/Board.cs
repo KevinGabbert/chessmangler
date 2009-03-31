708dce570833f2e751cc672de068b14d4295bf5b
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-03-31 03:58:35 +0000

    adding Nunit dlls that got left out

--- trunk/trunk/source/ClassLibrary1/Board.cs
+++ trunk/trunk/source/ClassLibrary1/Board.cs
@@ -1,95 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace Board
-{
-    public class square                   
-    {
-        public square(int x, int y)
-        {
-            int[] position = new int[2] { x, y };
-        }
-            public int black
-            {
-                get
-                { return black; }
-                set
-                { black = value; }
-            }
-            public int white
-            {
-                get
-                { return white; }
-                set
-                { white = value; }
-            }
-            public string name
-            {
-                get
-                { return name; }
-                set
-                { this.name = value; }
-            }
+//namespace Board
+//{
+//    public class Square                   
+//    {
+//        public Square(int x, int y)
+//        {
+//            int[] position = new int[2] { x, y };
+//        }
+//        public int black
+//        {
+//            get
+//            { return black; }
+//            set
+//            { black = value; }
+//        }
+//        public int white
+//        {
+//            get
+//            { return white; }
+//            set
+//            { white = value; }
+//        }
+//        public string name
+//        {
+//            get
+//            { return name; }
+//            set
+//            { this.name = value; }
+//        }
+//        public int number
+//        {
+//            get
+//            { return this.number; }
+//            set
+//            { this.number = value; }
+//        }
+//        public int color
+//        {
+//            get
+//            { return this.color; }
+//            set
+//            { this.color = value; }
+//        }
+//        public int piece
+//        {
+//            get
+//            { return this.piece; }
+//            set
+//            { piece = value; }
+//        }        
+//    }
 
-            public int number
-            {
-                get
-                { return this.number; }
-                set
-                { this.number = value; }
-            }
-            public int color
-            {
-                get
-                { return this.color; }
-                set
-                { this.color = value; }
-            }
+//    public class board
+//    {
+//        board(int x, int y)
+//        {
+//            // constructor takes two ints, makes the board
+//            Square[,] board = new Square[x, y];
 
+//            //initialize all squares created
 
-            public int piece
-            {
-                get
-                { return this.piece; }
-                set
-                { piece = value; }
-            }
-    
-            
-    }
-
-    public class board
-    {
-        board(int x, int y)
-        {
-            // constructor takes two ints, makes the board
-            square[,] board = new square[x, y];
-
-            //initialize all squares created
-
-            for (int row = 0; row == x; row++)
-            {
-                for (int column = 0; column == y; column++)
-                {
-                    board[row, column] = new square(row,column);
-                    board[row, column].piece = 0;
-                    board[row, column].black = 0;
-                    board[row, column].white = 0;
-                    board[row, column].number = (x*y)+x;
-                    board[row, column].name = (char)(97+row)+column.ToString();
+//            for (int row = 0; row == x; row++)
+//            {
+//                for (int column = 0; column == y; column++)
+//                {
+//                    board[row, column] = new Square(row,column);
+//                    board[row, column].piece = 0;
+//                    board[row, column].black = 0;
+//                    board[row, column].white = 0;
+//                    board[row, column].number = (x*y)+x;
+//                    board[row, column].name = (char)(97+row)+column.ToString();
                     
-                    if (((board[row, column].number)%2)==0)
-                    {board[row, column].color = 1;}
-                    else
-                    {board[row, column].color = 0;}
-                }
-            }           
+//                    if (((board[row, column].number)%2)==0)
+//                    {board[row, column].color = 1;}
+//                    else
+//                    {board[row, column].color = 0;}
+//                }
+//            }           
 
 
-        }
-
-
-    }     
-}
+//        }
+//    }     
+//}
