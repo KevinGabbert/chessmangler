084d4530d1bb8a8f8edab8a2d7f81a5b07ca96fa
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-01 21:00:47 +0000

    Added "ChessPiece" template (generated by VS)
    I guess this is a good name as we will only be focusing on Chess Variants..

---
+++ source/Rules/Rules/Types/ChessPiece.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rules.Interfaces;
+
+namespace Rules.Types
+{
+    public class ChessPiece: IConfigurablePiece
+    {
+        #region IConfigurablePiece Members
+
+        public Engine.Types.Location Location
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Drawing.Color Color
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public List<System.Drawing.Color> Team
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public AttackPackage Attacking
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public MovementPackage Moves
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CheckMy_MovementRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(long from, long to)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
