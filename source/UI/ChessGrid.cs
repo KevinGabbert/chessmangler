ee81d8df23fbe611013a2a3f90baa95feb3534ac
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-03 21:11:04 +0000

    Refactored prototype board into 2 new classes.  UIBoard, and UISquare
    These may get refactored further (or I might rename WinUIParts into WinUIEngine or something..
    
    but in the meantime, checking in what work I have so far...

--- source/UI/ChessGrid.cs
+++ source/UI/ChessGrid.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using WinUIParts;
+
 namespace SKChess
 {
     public partial class ChessGrid : Form
@@ -18,57 +20,10 @@ namespace SKChess
             InitializeComponent();
         }
 
-        public bool squareColor = false;
-
         private void ChessGrid_Load(object sender, EventArgs e)
         {
-            //throw down a bunch of pictureboxen
-
-            //*** mod Engine.CreateBoard so that it will use the same code to create this board ***
-
-            //This grid needs to bound to Board.
-
-            //Create a new class called square that inherits from picturebox, and adds 2 props
-            //1. Engine.Board.Square
-            //2. IConfigurablePiece
-
-
-            this.Width = 584;
-            this.Height = 606;
-
-            for (int i = 0; i < 576; i = i + 72)
-            {
-                for (int j = 0; j < 576; j = j + 72)
-                {
-                    //Square.SetColor(newSquare, column, row);
-                    //this.Squares.Add(newSquare);
-
-                    PictureBox newBox = new PictureBox();
-                    newBox.Location = new System.Drawing.Point(i, j);
-                    newBox.Name = "Square" + (i + j).ToString();
-                    newBox.Size = new System.Drawing.Size(72, 72);
-
-                    //it would be nice to enable keyboard support (depending on an option in the config file)
-                    //newBox.pictureBox1.TabIndex = 0;
-                    //newBox.pictureBox1.TabStop = false;
-
-                    newBox.Image = new Bitmap(Environment.CurrentDirectory + "\\images\\wr.gif");
-
-
-                    //Graphics gfx = Graphics.FromImage(newBox.Image);
-
-                    //clean up the image (take care of any image loss from resizing)
-
-                    //gfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
-
-                    newBox.SizeMode = PictureBoxSizeMode.CenterImage;
-                    newBox.BorderStyle = BorderStyle.FixedSingle;
-
-                    this.Controls.Add(newBox);
-                    this.squareColor = !this.squareColor;
-                }
-                this.squareColor = !this.squareColor;
-            }
+            UIBoard newBoard = new UIBoard();
+            newBoard.CreateBoard(this);  
         }
     }
 }
---
+++ source/WinUIParts/UISquare.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinUIParts
+{
+    public class UISquare : PictureBox
+    {
+        private UISquare()
+        {
+
+        }
+
+        public UISquare(int row, int column, int size, string picture)
+        {
+            //this = (UISquare) new PictureBox();
+            this.MakeSquare(row, column, size, picture);
+        }
+
+        private void MakeSquare(int row, int column, int size, string picture)
+        {
+            this.Location = new System.Drawing.Point(row, column);
+            this.Name = "Square" + (row + column).ToString();
+            this.Size = new System.Drawing.Size(size, size); //this *is* a square after all
+
+            //it would be nice to enable keyboard support (depending on an option in the config file)
+            //newBox.pictureBox1.TabIndex = 0;
+            //newBox.pictureBox1.TabStop = false;
+
+            this.Image = new Bitmap(picture);
+            this.SizeMode = PictureBoxSizeMode.CenterImage;
+            this.BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+}
