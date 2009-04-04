2911962b7e004324951dcb9202fbafc6cb8eee9c
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-04 05:46:38 +0000

    Threw down an app.config for the UI. (this could move to another object..) and some pseudocode for the configuration
    Threw in some pseudeCode to populate the UI Board's squares with pieces.
    
    implemented ISquare, sorta..

--- trunk/source/UI/ChessGrid.cs
+++ trunk/source/UI/ChessGrid.cs
@@ -4,12 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 using WinUIParts;
+
+using System.Configuration;
 
 namespace SKChess
 {
@@ -22,8 +22,18 @@ namespace SKChess
 
         private void ChessGrid_Load(object sender, EventArgs e)
         {
+            int rows = 8;
+            int columns = 8;
+            int squareSize = 72;
+
+            //ConfigurationManager.OpenExeConfiguration(System.Environment.CurrentDirectory
+
+            //int rows = Convert.ToInt16(ConfigurationManager.AppSettings["DefaultBoard_Rows"]);
+            //int columns = Convert.ToInt16(ConfigurationManager.AppSettings["DefaultBoard_Columns"]);
+            //int squareSize = Convert.ToInt16(ConfigurationManager.AppSettings["DefaultBoard_SquareSize"]);
+
             UIBoard newBoard = new UIBoard();
-            newBoard.CreateBoard(this);  
+            newBoard.CreateBoard(this, rows, columns, squareSize); //get these from XML file  
         }
     }
 }
