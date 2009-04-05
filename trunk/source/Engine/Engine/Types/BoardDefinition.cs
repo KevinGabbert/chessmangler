e9eecd9c955843a2119d18727c9f0ba2960489c6
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-05 07:24:03 +0000

    Created BoardDef object, and laid out some plans so we can get Square Data from a future Board.SquareLogic()

---
+++ trunk/source/Engine/Engine/Types/BoardDefinition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Types
+{
+    public class BoardDef
+    {
+        int _rows;
+        public int Rows
+        {
+            get
+            {
+                return _rows;
+            }
+            set
+            {
+                _rows = value;
+            }
+        }
+
+        int _cols;
+        public int Columns
+        {
+            get
+            {
+                return _cols;
+            }
+            set
+            {
+                _cols = value;
+            }
+        }
+    }
+}
