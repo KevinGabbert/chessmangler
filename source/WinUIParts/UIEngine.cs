c996f0ed40aa6fa81e3a026f5a969cea83378837
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-06 06:49:42 +0000

    screw it. Removed Rules DLL.
    I was having too many problems with circular references between the two.
    
    Whats more important is to have rules in a separate XML file, or separate section, and rules objects that can be loaded with any set of Rules
    
    actually, come to think of it, each Board type needs to have their own Rules prop..

--- source/WinUIParts/UIEngine.cs
+++ source/WinUIParts/UIEngine.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace WinUIParts
 {
     public class UIEngine
     {
-        public static bool IsThisMoveOkay(DGV_ChessSquare startSquare, DGV_ChessSquare endSquare)
-        {
-            //get start chesspiecetype
-            //string xxx = ((IConfigurablePiece)startSquare.Value).Type;
+        //public static bool IsThisMoveOkay(DGV_ChessSquare startSquare, DGV_ChessSquare endSquare)
+        //{
+        //    //get start chesspiecetype
+        //    //string xxx = ((IConfigurablePiece)startSquare.Value).Type;
 
-            //talk to Engine.  Find out if we can do the move.  if not, then we need to read the broken rules Collection
+        //    //talk to Engine.  Find out if we can do the move.  if not, then we need to read the broken rules Collection
 
 
 
-            return true;
-        }
+        //    return true;
+        //}
 
         //possible alternative:
         //public static bool IsThisMoveOkay(ChessSquare startSquare, ChessSquare endSquare, out BrokenRulesCollection)
