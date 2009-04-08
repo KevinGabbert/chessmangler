fdd1402ec8b8aa7a5fc58df974dbe73c2735cb4e
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-08 06:08:37 +0000

    Pieces can now be pulled from the config file (note: that config format is not final)
    also, moved board & piece population to Engine.csproj
    
    (however, this may have invalidated SquareLogic. (I didn't realize that it was being executed twice earlier.  Put in an IF statement in SquareLogic() to fix that.) 
    
    This also required the creation of Board.GetByLocation().  which is quite helpful.
    
    also, I busted all the tests with this latest refactor.  Ran out of time tonight so I will get them back online tomorrow.
    
    Basic UI Square and piece population is complete.  Next I'll need to do Drag & Drop. (again!)

--- trunk/source/Engine/Engine/Types/Boards/Board.cs
+++ trunk/source/Engine/Engine/Types/Boards/Board.cs
@@ -12,6 +12,9 @@ namespace Engine.Types
 
         public string _findSquareName;
 
+        public int _findRow;
+        public int _findCol;
+
         List<Square2D> _squares = new List<Square2D>();
         public List<Square2D> Squares
         {
@@ -38,7 +41,57 @@ namespace Engine.Types
             }
         }
 
+        bool _isNew = true;
+        public bool IsNew
+        {
+            get
+            {
+                return _isNew;
+            }
+            set
+            {
+                _isNew = value;
+            }
+        }
+
         #endregion
+
+        public Board2D(XmlDocument configFile)
+        {
+            this.CreateBoard(configFile);
+            this.SetPieces(configFile);
+        }
+
+        protected void CreateBoard(XmlDocument configFile)
+        {
+            BoardDef boardDef = this.GetBoardDef(configFile); // TODO: if BoardDef is null then throw custom exception
+            this.Definition = boardDef;
+
+            //This foreach construct may be temporary, as I'm evaluating whether this is a good idea..
+            foreach (Square2D currentSquare in this.SquareLogic(boardDef))
+            {
+                //this empty foreach executes SquareLogic. We may need to put something here later..
+            } 
+        }
+
+        protected void CreateBoardFromConfig(XmlDocument configFile)
+        {
+            //This is where all the Board and piece setup (from config file) needs to be done!
+        }
+        private void SetPieces(XmlDocument startingPosition)
+        {
+            List<PieceDef> piecesToSet = this.GetPieces(startingPosition);
+
+            foreach (PieceDef piece in piecesToSet)
+            {
+                Square2D squareForPiece = this.GetByName(piece.StartingLocation);
+                squareForPiece.CurrentPiece = new Piece(piece.Name);
+                squareForPiece.CurrentPiece.Image = new Bitmap(Environment.CurrentDirectory + "\\images\\" + piece.ImageName);
+            }
+        }
+
+
+        #region Squares
 
         public Square2D GetByName(string squareName)
         {
@@ -48,6 +101,16 @@ namespace Engine.Types
 
             return foundSquare;
         }
+        public Square2D GetByLocation(int row, int column)
+        {
+            //look up the square by its name.  ex. name:  "A2"
+            this._findRow = row;
+            this._findCol = column;
+            Square2D foundSquare = this.Squares.Find(foundByLocation);
+
+            return foundSquare;
+        }
+
         protected bool foundByName(Square2D find)
         {
             if (find.Name == this._findSquareName)
@@ -59,31 +122,17 @@ namespace Engine.Types
                 return false;
             }
         }
-
-        public Board2D(Int16 columns, Int16 rows, XmlDocument startingPosition)
+        protected bool foundByLocation(Square2D find)
         {
-            this.CreateBoard(columns, rows);
+            if ((find.Row == this._findRow) & (find.Column == this._findCol))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-        public Board2D(Int16 columns, Int16 rows)
-        {
-            this.CreateBoard(columns, rows);
-        }
-
-        protected void CreateBoard(Int16 columns, Int16 rows)
-        {
-            //BoardDef newBoardDef = new BoardDef();
-            //newBoardDef.Columns = columns;
-            //newBoardDef.Rows = rows;
-
-            //this.InitializeSquares(newBoardDef);
-            this.SquareLogicWithTestPiece(columns, rows);
-        }
-
-        protected void CreateBoardFromConfig(XmlDocument configFile)
-        {
-            //This is where all the Board and piece setup (from config file) needs to be done!
-        }
-
 
         protected static void SetSquareColor(Square2D squareToColor)
         {
@@ -96,7 +145,6 @@ namespace Engine.Types
                 squareToColor.Color = Color.White;
             }
         }
-
         public void AddNewSquare(BoardDef boardDef, Int16 col, Int16 row, out Square2D newlyAddedSquare)
         {
             Square2D newSquare = new Square2D();
@@ -106,36 +154,134 @@ namespace Engine.Types
             
             this.Squares.Add(newSquare);
             newlyAddedSquare = newSquare;
+
+            newSquare.Column = col;
+            newSquare.Row = row;
         }
 
         //Expose our inner board logic so the UI can use it.
-        //Remove "withTestPiece" once XML file is parsed.
-        public IEnumerable SquareLogicWithTestPiece(Int16 columns, Int16 rows)
+        public IEnumerable SquareLogic(BoardDef boardDef)
         {
-            for (Int16 currentRow = 0; currentRow < rows; currentRow++)
+            for (Int16 currentRow = 0; currentRow < boardDef.Rows; currentRow++)
             {
-                for (Int16 currentColumn = 0; currentColumn < columns; currentColumn++)
+                for (Int16 currentColumn = 0; currentColumn < boardDef.Columns; currentColumn++)
                 {
-                    BoardDef newBoardDef = new BoardDef();
-                    newBoardDef.Columns = columns;
-                    newBoardDef.Rows = rows;
+                    //This IF may be temporary, as I'm evaluating whether this is a good idea..
+                    if (this.IsNew)
+                    {
+                        Square2D newSquare;
+                        this.AddNewSquare(boardDef, currentColumn, currentRow, out newSquare);
 
-                    Square2D newSquare;
-                    this.AddNewSquare(newBoardDef, currentColumn, currentRow, out newSquare);
-
-                    newSquare.Column = currentColumn;
-                    newSquare.Row = currentRow;
-
-                    //TODO:  This needs to be set up from XML file
-                    //**** This is disposable test code, as the pieces will be set in Engine.Board (XmlDocument) *****
-                    newSquare.CurrentPiece = new Piece("Rook");
-                    newSquare.CurrentPiece.Image = new Bitmap(Environment.CurrentDirectory + "\\images\\wr.gif");
-                    //**** This is disposable test code, as the pieces will be set in Engine.Board (XmlDocument) *****
-
-
-                    yield return newSquare; //Hand out this current square to those who want it..
+                        yield return newSquare; //Hand out this new square to those who want it..
+                    }
+                    else
+                    {
+                        //This would normally be called by the UI via a for loop.
+                        yield return this.GetByLocation(currentRow, currentColumn);
+                    }
                 }
             }
+
+            this.IsNew = false;
         }
+
+        #endregion
+
+        #region Xml
+
+        public XmlNode GetDefNode(XmlDocument configFile, string defNode)
+        {
+            XmlNode gotDefNode = null;
+
+            foreach (XmlNode xmlNode in configFile)
+            {
+                if (xmlNode.Name == "ChessConfig")
+                {
+                    foreach (XmlNode childNode in xmlNode)
+                    {
+                        if (childNode.Name == defNode)
+                        {
+                            gotDefNode = childNode;
+                        }
+                    }
+                }
+            }
+
+            return gotDefNode;
+        }
+        public BoardDef GetBoardDef(XmlDocument configFile)
+        {
+            BoardDef gotBoardDef = null;
+
+            XmlNode boardDefNode = this.GetDefNode(configFile, "BoardDef");
+
+            if (boardDefNode != null)
+            {
+                gotBoardDef = new BoardDef();
+
+                XmlAttributeCollection attributes = boardDefNode.Attributes;
+                foreach (XmlAttribute currentAttribute in attributes)
+                {
+                    string currentName = currentAttribute.Name;
+
+                    if (currentName == "rows")
+                    {
+                        gotBoardDef.Rows = Convert.ToInt16(currentAttribute.Value);
+                    }
+
+                    if (currentName == "columns")
+                    {
+                        gotBoardDef.Columns = Convert.ToInt16(currentAttribute.Value);
+                    }
+                }
+            }
+
+            return gotBoardDef;
+        }
+        public List<PieceDef> GetPieces(XmlDocument startingPosition)
+        {
+            List<PieceDef> gotPieceDefs = null;
+
+            XmlNode pieceDefs = this.GetDefNode(startingPosition, "PieceDef");
+
+            if (pieceDefs != null)
+            {
+                gotPieceDefs = new List<PieceDef>();
+
+                foreach (XmlNode currentPiece in pieceDefs)
+                {
+                    if (currentPiece.Attributes != null)
+                    {
+                        PieceDef newPiece = new PieceDef();
+
+                        XmlAttributeCollection attributes = currentPiece.Attributes;
+                        foreach (XmlAttribute currentAttribute in attributes)
+                        {
+                            string currentName = currentAttribute.Name;
+
+                            if (currentName == "name")
+                            {
+                                newPiece.Name = currentAttribute.Value;
+                            }
+
+                            if (currentName == "StartingLocation")
+                            {
+                                newPiece.StartingLocation = currentAttribute.Value;
+                            }
+
+                            if (currentName == "ImageName")
+                            {
+                                newPiece.ImageName = currentAttribute.Value;
+                            }
+                        }
+                        gotPieceDefs.Add(newPiece);
+                    }
+                }
+            }
+
+            return gotPieceDefs;
+        }
+
+        #endregion
     }
 }
