72d2099e0dbe2e3e7373a68626e9077cdfa5d016
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-10 05:54:31 +0000

    Laying out some classes to support the new proposed rules.
    Most notably is Movement.  All Movements require a condition, even if the condition == "none"
    
    See my note in the Movement class..

---
+++ source/Engine/Engine/Types/Rules/Condition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Engine.Interfaces;
+
+namespace Engine.Types.Rules
+{
+    public class Condition
+    {
+        private bool _override;
+        public bool Override
+        {
+            get
+            {
+                return _override;
+            }
+            set
+            {
+                _override = value;
+            }
+        }
+
+        private ConditionType _type;
+        public ConditionType Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        private List<IPiece> _applyTo;
+        public List<IPiece> ApplyTo
+        {
+            get
+            {
+                return _applyTo;
+            }
+            set
+            {
+                _applyTo = value;
+            }
+        }
+    }
+}
