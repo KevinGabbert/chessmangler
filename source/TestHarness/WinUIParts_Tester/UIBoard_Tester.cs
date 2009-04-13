fd4ab2079b20617423dd75f3018cee877b2f6476
kevingabbert <kevingabbert@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-04-13 05:33:20 +0000

    committing what Tortoise forgot

---
+++ source/TestHarness/WinUIParts_Tester/UIBoard_Tester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+using System.Windows.Forms;
+using NUnit.Framework;
+using WinUIParts;
+
+namespace TestHarness.WinUIParts_Tester
+{
+    [TestFixture]
+    public class UIBoard_Tester
+    {
+        XmlDocument _testSetup = new XmlDocument();
+
+        string _sourceDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).ToString()).ToString()).ToString();
+        string _uiDirectory;
+        string _imagesDirectory;
+        string _configFile;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uiDirectory = _sourceDir + "\\UI";
+            _imagesDirectory = _uiDirectory + "\\images";
+            _configFile = _sourceDir + "\\UI\\Board2D.config";
+
+            //before we start, lets verify everything..
+            Assert.IsTrue(File.Exists(_configFile), "can't find config file! TestHarness looked here: " + _configFile);
+            Assert.IsTrue(Directory.Exists(_uiDirectory), "can't find UI Directory! TestHarness looked here: " + _configFile);
+            Assert.IsTrue(Directory.Exists(_imagesDirectory), "can't find Images Directory! TestHarness looked here: " + _uiDirectory + "\\images");
+
+            _testSetup = Config.LoadXML(_configFile);
+        }
+
+        [Test]
+        public void test()
+        {
+            UIBoard newBoard = new UIBoard();
+            newBoard.CreateBoard(new Form(), _testSetup, _uiDirectory); //get these from XML file 
+        }
+    }
+}
