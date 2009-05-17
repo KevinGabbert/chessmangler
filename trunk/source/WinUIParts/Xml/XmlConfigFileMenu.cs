350cf12c0b2c6491575533571513ae9d12088538
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-05-17 08:34:38 +0000

    refs #72,
    implemented a "cache" to store PieceDefs as we read them from the XML File. I thought I'd get creative and store them in a custom MenuItem, however, that wound up to be a bit more complex than I wanted.  A simple dictionary with unique names is far easier!

--- trunk/source/WinUIParts/Xml/XmlConfigFileMenu.cs
+++ trunk/source/WinUIParts/Xml/XmlConfigFileMenu.cs
@@ -17,6 +17,7 @@ namespace ChessMangler.WinUIParts.Xml
     public class XmlConfigFileMenu
     {
         UISquare _clickedSquare;
+        Dictionary<string, PieceDef> _tempCache = new Dictionary<string, PieceDef>();
 
         //TODO: put this in a "Common Menu Tasks" object
         public static MenuItem NewMenuItem(string caption, string name)
@@ -62,16 +63,20 @@ namespace ChessMangler.WinUIParts.Xml
                     XmlDocument rulesDocument = Config.LoadXML(file);
                     List<PieceDef> piecesToSet = ConfigParser.GetUniquePieces(rulesDocument);
 
-                    foreach (PieceDef piece in piecesToSet)
+                    foreach (PieceDef pieceDef in piecesToSet)
                     {
-                        switch (piece.Name)
+                        switch (pieceDef.Name)
                         {
                             case "All":
                                 break;
 
                             default:
-                                configFileMenu.MenuItems.Add(NewMenuItem(piece.Name, piece.Name + "Piece"));
-                                configFileMenu.MenuItems[piece.Name + "Piece"].Click += pieceMenuItem_Click;
+                                string uniqueName = pieceDef.Name + Guid.NewGuid();
+                                configFileMenu.MenuItems.Add(NewMenuItem(pieceDef.Name, uniqueName));
+                                configFileMenu.MenuItems[uniqueName].Click += pieceMenuItem_Click;
+
+                                //Store what we've read in, cause we are going to look up what the user selected..
+                                this._tempCache.Add(uniqueName, pieceDef); 
                                 break;
                         }
                     }
@@ -98,6 +103,7 @@ namespace ChessMangler.WinUIParts.Xml
                 //first of all, what Piece was clicked?  Because we can create a new instance of it if needed
 
                 Piece newPiece = new Piece();
+                newPiece.Definition = this.GetPieceDef(currentMenuItem.Name);
 
                 newPiece.Row = this._clickedSquare.Row;
                 newPiece.Column = this._clickedSquare.Column;
@@ -109,7 +115,7 @@ namespace ChessMangler.WinUIParts.Xml
                 //newPiece.Image;
                 //newPiece.Color;
                 //newPiece.Name;
-                
+
 
                 this._clickedSquare.CurrentPiece = newPiece;
             }
@@ -121,252 +127,13 @@ namespace ChessMangler.WinUIParts.Xml
 
             int x;
         }
+
+        private PieceDef GetPieceDef(string name)
+        {
+            PieceDef gotValue;
+
+            _tempCache.TryGetValue(name, out gotValue);
+            return gotValue;
+        }
     }
 }
-
-
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-
-//using System.IO;
-//using System.Xml;
-
-//using System.Windows.Forms;
-//using ChessMangler.Engine.Types;
-//using ChessMangler.Engine.Config;
-
-//namespace ChessMangler.WinUIParts.Xml
-//{
-//    /// <summary>
-//    /// The purpose of this class is to build a Windows.Form.MenuItem tree containing piece names & images from each stored Config File
-//    /// </summary>
-//    public class XmlConfigFileMenu
-//    {
-//        UISquare _clickedSquare;
-
-//        PieceDef _selectedPieceDef;
-//        public PieceDef SelectedPieceDef
-//        {
-//            get
-//            {
-//                return _selectedPieceDef;
-//            }
-//            set
-//            {
-//                _selectedPieceDef = value;
-//            }
-//        }
-
-//        //TODO: put this in a "Common Menu Tasks" object
-//        public static MenuItem NewMenuItem(string caption, string name)
-//        {
-//            MenuItem addPieceMenu = new MenuItem();
-//            addPieceMenu.Name = name;
-//            addPieceMenu.Text = caption;
-
-//            return addPieceMenu;
-//        }
-
-//        //TODO: put this in a "Common Menu Tasks" object
-//        public static PieceMenuItem NewPieceMenuItem(string caption, string name)
-//        {
-//            PieceMenuItem addPieceMenu = new PieceMenuItem();
-//            addPieceMenu.Name = name;
-//            addPieceMenu.Text = caption;
-
-//            return addPieceMenu;
-//        }
-
-//        //Proof of concept
-//        //TODO: Refactor this
-//        public void Build_ConfigFile_PieceMenu(UISquare clickedSquare)
-//        {
-//            PieceMenuItem configFileMenu = new PieceMenuItem();
-
-//            this._clickedSquare = clickedSquare;
-
-//            MenuItem addPieceFromMenu = NewMenuItem("Add Piece from: ", "AddPieceFromMenu");
-//            clickedSquare.ContextMenu.MenuItems.Add(addPieceFromMenu);
-
-//            try
-//            {
-//                //Looks through config directory, and list what Config files are found
-//                string configDir = Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).ToString()).ToString() + "\\Config";
-
-//                List<string> configFiles = new List<string>();
-                
-//                if (Directory.Exists(configDir))
-//                {
-//                    foreach (string file in Directory.GetFiles(configDir, "*.config"))
-//                    {
-//                        configFiles.Add(file);
-//                    }
-//                }
-
-//                foreach (string file in configFiles)
-//                {
-//                    string name = Path.GetFileName(file).Replace(".config", "");
-
-//                    configFileMenu = (PieceMenuItem)NewPieceMenuItem(name, name + "ConfigFile");
-
-//                    XmlDocument rulesDocument = Config.LoadXML(file);
-//                    List<PieceDef> piecesToSet = ConfigParser.GetUniquePieces(rulesDocument);
-
-//                    foreach (PieceDef pieceDef in piecesToSet)
-//                    {
-//                        switch (pieceDef.Name)
-//                        {
-//                            case "All":
-//                                break;
-
-//                            default:
-//                                PieceMenuItem newItem = (PieceMenuItem)NewPieceMenuItem(pieceDef.Name, pieceDef.Name + "Piece");
-//                                newItem.Click += pieceMenuItem_Click;
-//                                newItem.PieceDef = pieceDef;
-
-//                                configFileMenu.MenuItems.Add(newItem);
-                               
-//                                //configFileMenu.MenuItems[pieceDef.Name + "Piece"].PieceDef = pieceDef;
-//                                //configFileMenu.MenuItems[pieceDef.Name + "Piece"].Click += pieceMenuItem_Click;
-//                                break;
-//                        }
-//                    }
-
-//                    clickedSquare.ContextMenu.MenuItems[addPieceFromMenu.Name].MenuItems.Add(configFileMenu);
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-
-//            }
-//        }
-
-//        public void pieceMenuItem_Click(object sender, EventArgs e)
-//        {
-//            MenuItem currentMenuItem;
-
-//            try
-//            {
-//                currentMenuItem = (MenuItem)sender;
-
-//                //This is called for every clicked Piece..
-
-//                //first of all, what Piece was clicked?  Because we can create a new instance of it if needed
-
-//                Piece newPiece = new Piece();
-//                newPiece.Row = this._clickedSquare.Row;
-//                newPiece.Column = this._clickedSquare.Column;
-//                //newPiece.Definition = ?;
-//                //newPiece.Movement = ?;
-//                //newPiece.Capture = ?;
-//                //newPiece.
-
-//                //newPiece.Image;
-//                //newPiece.Color;
-//                //newPiece.Name;
-                
-//                this._clickedSquare.CurrentPiece = newPiece;
-//            }
-//            catch (Exception ex)
-//            {
-
-//            }
-//        }
-//    }
-
-//    public class PieceMenuItem : MenuItem
-//    {
-//        PieceMenuItemCollection _menuItems;
-//        public new PieceMenuItemCollection MenuItems
-//        {
-//            get
-//            {
-//                return _menuItems;
-//            }
-//            set
-//            {
-//                _menuItems = value;
-//            }
-//        }
-
-//        PieceDef _pieceDef;
-//        public PieceDef PieceDef
-//        {
-//            get
-//            {
-//                return _pieceDef;
-//            }
-//            set
-//            {
-//                _pieceDef = value;
-//            }
-//        }
-
-//        public PieceMenuItem()
-//        {
-//            this._menuItems = new PieceMenuItemCollection(this);
-//        }
-//   }
-
-//    public class PieceMenuItemCollection : Menu.MenuItemCollection
-//    {
-//        private List<PieceMenuItem> _list = new List<PieceMenuItem>();
-//        public List<PieceMenuItem> List
-//        {
-//            get
-//            {
-//                return _list;
-//            }
-//            set
-//            {
-//                _list = value;
-//            }
-//        }
-
-//        public PieceMenuItemCollection(Menu owner) : base(owner)
-//        {
-//        }
-
-//        public new PieceMenuItem this[int index]
-//        {
-//            get
-//            {
-//                return _list[index]; 
-//            }
-//            set
-//            {
-//                _list[index] = value;
-//            }
-//        }
-//        public int IndexOf(PieceMenuItem item)
-//        {
-//            return this.List.IndexOf(item);
-//        }
-//        public void Add(PieceMenuItem item)
-//        {
-//            this.List.Add(item);
-//        }
-//        public void Remove(PieceMenuItem item)
-//        {
-//            this.List.Remove(item);
-//        }
-//        public void CopyTo(PieceMenuItem[] array, int index)
-//        {
-//            this.List.CopyTo(array, index);
-//        }
-//        public void AddRange(PieceMenuItem[] collection)
-//        {
-//            this.AddRange(collection);
-//        }
-//        public bool Contains(PieceMenuItem item)
-//        {
-//            return this.List.Contains(item);
-//        }
-//        public void Insert(int index, PieceMenuItem item)
-//        {
-//            this.List.Insert(index, item);
-//        }
-//    }
-//}
-
