bb06ec33ef119b001d61e9c9fbea2eb2344cca34
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-05-18 06:27:39 +0000

    refs #72,
    refactor.  Committing before I screw it up.

--- trunk/source/WinUIParts/Menus/UniquePieceMenu.cs
+++ trunk/source/WinUIParts/Menus/UniquePieceMenu.cs
@@ -33,58 +33,9 @@ namespace ChessMangler.WinUIParts.Menus
         //TODO: Refactor this
         public void Build_ConfigFile_PieceMenu(UISquare clickedSquare)
         {
-            UniquePieceMenu._clickedSquare = clickedSquare;
-            MenuItem configFileMenu;
-
-            MenuItem addPieceFromMenu = NewMenuItem("Add Piece from: ", "AddPieceFromMenu");
-            clickedSquare.ContextMenu.MenuItems.Add(addPieceFromMenu);
-
             try
             {
-                //Looks through config directory, and list what Config files are found
-                string configDir = Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).ToString()).ToString() + "\\Config";
-
-                List<string> configFiles = new List<string>();
-
-                if (Directory.Exists(configDir))
-                {
-                    foreach (string file in Directory.GetFiles(configDir, "*.config"))
-                    {
-                        configFiles.Add(file);
-                    }
-                }
-
-                foreach (string filePath in configFiles)
-                {
-                    string fileNameOnly = Path.GetFileName(filePath).Replace(".config", "");
-
-                    configFileMenu = NewMenuItem(fileNameOnly, fileNameOnly + "ConfigFile");
-
-                    List<PieceDef> piecesToSet = ConfigParser.GetUniquePieces(Config.LoadXML(filePath));
-
-                    foreach (PieceDef pieceDef in piecesToSet)
-                    {
-                        switch (pieceDef.Name)
-                        {
-                            case "All":
-                                break;
-
-                            default:
-                                string uniqueName = pieceDef.Name + Guid.NewGuid();
-                                configFileMenu.MenuItems.Add(NewMenuItem(pieceDef.Name, uniqueName));
-                                configFileMenu.MenuItems[uniqueName].Click += pieceMenuItem_Click;
-
-                                //TODO:  Temporary Code (as this will be eventually pulled from a config file)
-                                pieceDef.ImageDirectory = Directory.GetParent(Path.GetDirectoryName(filePath)).FullName + @"\images\";
-
-                                //Store what we've read in, cause we are going to look up what the user selected..
-                                UniquePieceMenu._tempCache.Add(uniqueName, pieceDef); 
-                                break;
-                        }
-                    }
-
-                    clickedSquare.ContextMenu.MenuItems[addPieceFromMenu.Name].MenuItems.Add(configFileMenu);
-                }
+                this.Add_Config_Menus(clickedSquare);
             }
             catch (Exception ex)
             {
@@ -92,6 +43,82 @@ namespace ChessMangler.WinUIParts.Menus
                 //file is corrupt or missing.
             }
         }
+
+        public void Add_Config_Menus(UISquare clickedSquare)
+        {
+            UniquePieceMenu._clickedSquare = clickedSquare;
+            MenuItem configFileMenu;
+
+            MenuItem addPieceFromMenu = NewMenuItem("Add Piece from: ", "AddPieceFromMenu");
+            clickedSquare.ContextMenu.MenuItems.Add(addPieceFromMenu);
+
+            //Looks through config directory, and list what Config files are found
+            string configDir = Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).ToString()).ToString() + "\\Config";
+
+            List<string> configFiles = new List<string>();
+            UniquePieceMenu.Get_Config_Files(configDir, configFiles);
+
+            foreach (string filePath in configFiles)
+            {
+                string fileNameOnly = Path.GetFileName(filePath).Replace(".config", "");
+                configFileMenu = NewMenuItem(fileNameOnly, fileNameOnly + "ConfigFile");
+
+                this.Add_Piece_Menu(clickedSquare, configFileMenu, addPieceFromMenu, filePath);
+            }
+        }
+        public void Add_Piece_Menu(UISquare clickedSquare, MenuItem configFileMenu, MenuItem addPieceFromMenu, string filePath)
+        {
+            //This is the main focus of this function... Getting the unique pieces
+            List<PieceDef> piecesToSet = ConfigParser.GetUniquePieces(Config.LoadXML(filePath));
+
+            foreach (PieceDef pieceDef in piecesToSet)
+            {
+                switch (pieceDef.Name)
+                {
+                    case "All":
+                        break;
+
+                    default:
+                        Add_Piece_MenuItem(configFileMenu, filePath, pieceDef);
+                        break;
+                }
+            }
+
+            clickedSquare.ContextMenu.MenuItems[addPieceFromMenu.Name].MenuItems.Add(configFileMenu);
+        }
+        public void Add_Piece_MenuItem(MenuItem configFileMenu, string filePath, PieceDef pieceDef)
+        {
+            string uniqueName = pieceDef.Name + Guid.NewGuid();
+            configFileMenu.MenuItems.Add(NewMenuItem(pieceDef.Name, uniqueName));
+            configFileMenu.MenuItems[uniqueName].Click += pieceMenuItem_Click;
+
+            //TODO:  Temporary Code (as this will be eventually pulled from a config file)
+            pieceDef.ImageDirectory = Directory.GetParent(Path.GetDirectoryName(filePath)).FullName + @"\images\";
+
+            //Store what we've read in, cause we are going to look up what the user selected..
+            UniquePieceMenu._tempCache.Add(uniqueName, pieceDef);
+        }
+
+        public static void Get_Config_Files(string configDir, List<string> configFiles)
+        {
+            if (Directory.Exists(configDir))
+            {
+                foreach (string file in Directory.GetFiles(configDir, "*.config"))
+                {
+                    configFiles.Add(file);
+                }
+            }
+        }
+
+        private PieceDef GetPieceDef(string name)
+        {
+            PieceDef gotValue;
+
+            _tempCache.TryGetValue(name, out gotValue);
+            return gotValue;
+        }
+
+        #region Event Handlers
 
         public void pieceMenuItem_Click(object sender, EventArgs e)
         {
@@ -123,12 +150,6 @@ namespace ChessMangler.WinUIParts.Menus
             }
         }
 
-        private PieceDef GetPieceDef(string name)
-        {
-            PieceDef gotValue;
-
-            _tempCache.TryGetValue(name, out gotValue);
-            return gotValue;
-        }
+        #endregion
     }
 }
