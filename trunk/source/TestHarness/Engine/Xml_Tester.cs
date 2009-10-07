e9c25a4c2d3d99b8c7bd9863671177ac28e2b47e
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-10-07 06:07:31 +0000

    refs #161, #168,
    been out of the loop for a while.  #161 indicates this test doesn't work yet and there have been no changes since. 
    
    Ignoring for now (until I catch up on LinqToXml)
    
    adding Config folder and test file for when I do get it working..

--- trunk/source/TestHarness/Engine/Xml_Tester.cs
+++ trunk/source/TestHarness/Engine/Xml_Tester.cs
@@ -15,16 +15,18 @@ namespace ChessMangler.TestHarness.Engine
     [TestFixture]
     public class Xml_Tester
     {
-
+        [Ignore]
         [Test]
         public void Peek()
         {
             string configDir = Directory.GetParent(Directory.GetParent(System.Environment.CurrentDirectory).ToString()).ToString() + "\\Config";
-            string configFilePath = configDir + "\\TestConfig.config";
+            string configFilePath = configDir + "\\proposed.config";
 
             Assert.IsTrue(File.Exists(configFilePath), "can't find test config file");
 
-            XElement peekd = XmlParser.Peek(configFilePath, "Chessmangler", "version");
+            XElement peekd = XmlParser.Peek(configFilePath, "ChessMangler", "version");
+
+            Assert.IsNotNull(peekd, "This means it didn't find the node..");
             Assert.AreEqual("testversion", peekd.Value.ToString());
         }
     }
