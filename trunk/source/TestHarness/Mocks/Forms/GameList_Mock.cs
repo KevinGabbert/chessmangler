a2ec06ca5de24d720958c11e1b39ec63473cdf02
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-10-26 08:00:30 +0000

    #refs #170,
    missed these

---
+++ trunk/source/TestHarness/Mocks/Forms/GameList_Mock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessMangler.TestHarness.Mocks.Forms
+{
+    public class GameList_Mock : ChessMangler.WinUIParts.GameList
+    {
+    }
+}
---
+++ trunk/source/TestHarness/WinUIParts_Tester/GameList/GameList_Tester.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+using ChessMangler.Options.Interfaces;
+using ChessMangler.WinUIParts;
+using ChessMangler.TestHarness.Mocks.Forms;
+
+using NUnit.Framework;
+
+namespace ChessMangler.TestHarness.WinUIParts_Tester.GameList
+{
+    [TestFixture]
+    public class GameList_Tester
+    {
+        [Test]
+        public void newForm()
+        {
+            ChessMangler.WinUIParts.GameList x = new ChessMangler.WinUIParts.GameList();
+
+            Assert.IsInstanceOfType(typeof(ChessMangler.WinUIParts.GameList), x);       
+        }
+
+        [Test]
+        public void OpenForm2()
+        {
+            ChessMangler.WinUIParts.GameList x = new ChessMangler.WinUIParts.GameList();
+            x.Show();
+            x.Close(); 
+        }
+    }
+}
