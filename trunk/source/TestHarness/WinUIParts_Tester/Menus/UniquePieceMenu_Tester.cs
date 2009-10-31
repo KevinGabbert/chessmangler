fe489a8738e50fb903e22715c101c22c123810c1
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-10-31 07:13:51 +0000

    refs #170,
    a quick test for the night.
    
    Added menu tester

---
+++ trunk/source/TestHarness/WinUIParts_Tester/Menus/UniquePieceMenu_Tester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using System.Windows.Forms;
+
+using ChessMangler.WinUIParts.Menus;
+
+namespace ChessMangler.TestHarness.WinUIParts_Tester.Menus
+{
+    [TestFixture]
+    public class UniquePieceMenu_Tester
+    {
+        [Test]
+        public void CreateObject()
+        {
+            UniquePieceMenu x = new UniquePieceMenu();
+            Assert.IsInstanceOfType(typeof(UniquePieceMenu), x);
+        }
+
+
+        //This guy should move to another object
+        [Test]
+        public void NewMenuItem()
+        {
+            UniquePieceMenu x = new UniquePieceMenu();
+
+            MenuItem y = x.NewMenuItem("testMenuitem", "Fred");
+
+            Assert.AreEqual(y.Name, "Fred");
+            Assert.AreEqual(y.Text, "testMenuitem");
+        }
+
+        [Ignore]
+        [Test]
+        public void GetPieceDef()
+        {
+            //mock object and load tempcache
+
+           // call GetPieceDef
+        }
+    }
+}
