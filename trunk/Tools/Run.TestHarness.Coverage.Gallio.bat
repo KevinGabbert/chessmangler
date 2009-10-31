c036bd6dddbc8e4e3bd259cc6b6acd92346398c8
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-10-31 07:21:28 +0000

    closes #179,
    lets see what this does

--- trunk/Tools/Run.TestHarness.Coverage.Gallio.bat
+++ trunk/Tools/Run.TestHarness.Coverage.Gallio.bat
@@ -1,1 +1,2 @@
+DEL ChessMangler_TestHarnessCoverage.xml
 NCover\ncover.console.exe //reg //x ChessMangler_TestHarnessCoverage.xml Gallio\Gallio.Echo.exe ..\Source\TestHarness\bin\Debug\ChessMangler.TestHarness.dll /r:Local
