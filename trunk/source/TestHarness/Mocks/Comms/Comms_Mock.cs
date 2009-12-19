5782f5d8be12fa397e7d494befb7e5c9fa1caaa4
kevin@kevingabbert.com <kevin@kevingabbert.com@f4d5e77e-b309-11de-9e8c-a981578610da>
2009-12-19 09:20:37 +0000

    refs #170,
    got some simple object tests on ChessGrid2D.

---
+++ trunk/source/TestHarness/Mocks/Comms/Comms_Mock.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using jabber.client;
+using ChessMangler.Communications.Interfaces;
+
+namespace ChessMangler.TestHarness.Mocks.Comms
+{
+    public class Comms_Mock : ICommsHandler
+    {
+        #region ICommsHandler Members
+
+        public object originalHandler
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event ChessMangler.Communications.Handlers.Comms_Authenticate authenticate;
+
+        public event ChessMangler.Communications.Handlers.OpponentMove_Handler OpponentMove_Recieved;
+
+        public event ChessMangler.Communications.Handlers.OpponentChat OpponentChat_Recieved;
+
+        public void On_Authenticate(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void On_OpponentChat_RCV(string chatMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void On_OpponentMove(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string opponent, System.Xml.XmlElement stuffToWrite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Message(string to, string body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RequestOpponentCurrentGameVersion(string opponentName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RequestOpponentCMVersion(string opponentName)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/source/TestHarness/WinUIParts_Tester/ChessGrid2D/ChessGrid2D_Form_Tester.cs
@@ -0,0 +1,18 @@
+﻿using ChessMangler.WinUIParts.ChessGrid2D;
+using NUnit.Framework;
+using jabber.client;
+using ChessMangler.TestHarness.Mocks.Comms;
+
+namespace ChessMangler.TestHarness.WinUIParts_Tester.ChessGrid2D
+{
+    [TestFixture]
+    public class ChessGrid2D_Form_Tester
+    {
+        [Test]
+        public void ChessGrid2D_Object()
+        {
+            ChessGrid2D_Form x = new ChessGrid2D_Form(new PresenceManager(), new Comms_Mock(), "opponentName", "vtest");
+            Assert.IsInstanceOfType(typeof(ChessGrid2D_Form), x);
+        }
+    }
+}
